288c65ee05a3378f0cec6e4bf048ed7e4a2e3f46
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2024-01-17 17:20:06 -0300

    Testando SEO e metadata estatica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,43 @@
-import type { Metadata } from 'next'
-import { Red_Hat_Display } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Red_Hat_Display } from "next/font/google";
+import "./globals.css";
+import { url } from "inspector";
 
 const redHat = Red_Hat_Display({
-  weight: ["400", "500", "600", "800", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--Red-Hat",
+    weight: ["400", "500", "600", "800", "700", "900"],
+    style: ["normal", "italic"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--Red-Hat",
 });
 
 export const metadata: Metadata = {
-  title: 'Testezin em next',
-  description: 'Primeiro projeto em next',
-}
+    title: "Lil Next text",
+    description: "Primeiro projeto em next",
+    openGraph: {
+        title: "Lil Next text",
+        description: "Primeiro projeto em next",
+        url: "Mywebsiteinnext.com",
+        siteName: "Nequistih",
+        images: [
+            {
+                url: "/next.svg",
+                width:1260, 
+                height:800,
+            },
+        ],
+        locale: 'pt-BR'
+    },
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body className={redHat.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html>
+            <body className={redHat.className}>{children}</body>
+        </html>
+    );
 }
